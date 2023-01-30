91ccddf0befb318781553afb651233a5b7f6335a
gABRIEL01TAT <122890297+gABRIEL01TAT@users.noreply.github.com>
2023-01-30 17:33:41 +0100

    ExerciseXP-Gold

---
+++ index2.py
@@ -0,0 +1,16 @@
+print("Hello World\n " *4 ,"I love Python\n"  *4)
+
+Month = int(input("Enter a number of a month  "))
+if Month == (3>=5):
+    print("Spring")
+else :
+    if Month == (6>=8):
+        print("Summer")
+    else:
+        if Month == (9>=11):
+            print("Autum")
+        else:
+            if Month == (12>=2):
+                print("Winter")
+            else:
+                print("Not A Month  !!")
