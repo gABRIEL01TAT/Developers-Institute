af52e10aa1bcf35705afc9bc56bfb294cde5305f
gABRIEL01TAT <122890297+gABRIEL01TAT@users.noreply.github.com>
2023-01-30 17:17:00 +0100

    Exercise XP

---
+++ index.py
@@ -0,0 +1,50 @@
+My_age = 30
+result = My_age + 123879
+
+print(result)
+
+first_name = "Ambi Gabriel"
+last_name = "Tata"
+print(first_name +" "+ last_name)
+
+print("Hello Word\n " *4)
+
+result = (99^3) *8
+print(result)
+
+computer_brand = "lenovo"
+print("I have a"+" "+computer_brand+" "+"computer")
+
+__name__ = "Gabriel"
+Age = str('19')
+shoe_sieze = str('41')
+info = "MY name is "+" "+__name__ +" "+"I am"+" "+ Age +" "+"And my show sieze is "+" "+ shoe_sieze
+print(info)
+
+a = 9
+b = 6
+if a > b:
+    print("Hello World")
+else:
+    print("")
+
+
+a = int(input("Enter a number  ")) 
+if a % 2==0:
+    print("The number is even")
+else:
+    print("The number is odd......!")
+
+
+c = input("Enter your name    ")
+if c=="Ambi gabriel":
+    print("Your ryt")
+else:
+    print("Get out of here you imposter stupid thieve")
+
+d = float(input("Enter your Height  "))
+Height = d * 2.54
+if Height==145:
+    print("congrats you are tall enough for a ride , Enjoy!!!!!")
+else:
+    print("Sorry you neeed to grow some more")
